dd06652521cd4e4ac9c9c29efa0dbbe57f4bbf3a
JC-MB <barkatu.83@gmail.com>
2016-03-01 13:45:44 -0600

    products requests specs with header

--- spec/requests/api/v1/products_spec.rb
+++ spec/requests/api/v1/products_spec.rb
@@ -2,10 +2,21 @@ require 'rails_helper'
 
 RSpec.describe "Products", :type => :request do
 
+  let(:user) {
+    nil #FactoryGirl.create(:user)
+  }
+
+  let(:auth_header) do
+    {
+      :'Accept' => "application/vnd.pagination-example.com+json; version=1",
+      :'Authorization' => ActionController::HttpAuthentication::Token.encode_credentials('USER_AUTH_TOKEN', { email: 'user@email.com' })
+    }
+  end
+
   describe "GET /products" do
     it 'Works!' do
 
-      get api_products_path
+      get api_products_path, nil, auth_header
       # or
       # get '/api/products'
 
@@ -17,7 +28,7 @@ RSpec.describe "Products", :type => :request do
     it 'sends a list of products' do
       FactoryGirl.create_list(:fruit, 10)
 
-      get api_products_path
+      get api_products_path, nil, auth_header
 
       # parse the json response
       json = JSON.parse(response.body)
@@ -31,7 +42,7 @@ RSpec.describe "Products", :type => :request do
 
     it 'Works!' do
 
-      get api_product_path(product)
+      get api_product_path(product), nil, auth_header
       # test for the 200 status-code
       expect(response).to be_success
     end
@@ -39,7 +50,7 @@ RSpec.describe "Products", :type => :request do
     it 'Show the recient created product' do
       FactoryGirl.create_list(:fruit, 10)
 
-      get api_product_path(product)
+      get api_product_path(product), nil, auth_header
 
       # parse the json response
       json = JSON.parse(response.body)
