14b499abbd01471dc33077ca5854d77d82502ea3
JC-MB <barkatu.83@gmail.com>
2016-03-01 12:48:22 -0600

    products controller clean version

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::BaseController < ApplicationController
   protect_from_forgery with: :null_session
 
-  before_filter :check_format
+  before_action :check_format
 
   def check_format
     render :nothing => true, :status => 406 unless params[:format] == 'json' || request.headers["Accept"] =~ /json/
---
+++ app/controllers/api/v1/products_controller.rb
@@ -0,0 +1,48 @@
+class Api::V1::ProductsController < ApplicationController
+  before_action :set_product, only: [:show, :edit, :update, :destroy]
+
+  # GET /products
+  def index
+    @products = Product.all
+    render json: @products
+  end
+
+  # GET /products/1
+  def show
+    render json: @product
+  end
+
+  # POST /products
+  def create
+    @product = Product.new(product_params)
+
+    if @product.save
+      render json: @product, status: :created
+    else
+      render json: @product.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /products/1
+  def update
+    if @product.update(product_params)
+      render json: @product
+    else
+      render json: @product.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @product.destroy
+    head :no_content
+  end
+
+  private
+    def set_product
+      @product = Product.find(params[:id])
+    end
+
+    def product_params
+      params.require(:product).permit(:name, :description, :price)
+    end
+end
