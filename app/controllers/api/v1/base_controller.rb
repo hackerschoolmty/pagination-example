87f95ca9a739eca1d5029d1e08fb29073a2486bb
JC-MB <barkatu.83@gmail.com>
2016-03-01 12:30:10 -0600

    respond only to json requests in api namespace

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,4 +1,9 @@
 class Api::V1::BaseController < ApplicationController
   protect_from_forgery with: :null_session
 
+  before_filter :check_format
+
+  def check_format
+    render :nothing => true, :status => 406 unless params[:format] == 'json' || request.headers["Accept"] =~ /json/
+  end
 end
